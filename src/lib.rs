8e93ffc913b009af5565e55c9f6aa0ca8918e064
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-02-16 20:11:23 +0100

    Initial version of union-find over polygons, still non-functional

---
+++ src/lib.rs
@@ -0,0 +1,186 @@
+// SPDX-FileCopyrightText: 2026 polygon_unionfind contributors
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use std::marker::PhantomData;
+
+use i_overlay::{
+    core::{fill_rule::FillRule, overlay_rule::OverlayRule},
+    float::single::SingleFloatOverlay,
+    i_float::float::compatible::FloatPointCompatible,
+};
+
+use maplike::{Get, Insert, KeyedCollection, Push, Set};
+use num_traits::{FromPrimitive, ToPrimitive};
+use rstar::{
+    AABB, Envelope, RTree, RTreeNum, RTreeObject,
+    primitives::{GeomWithData, Rectangle},
+};
+use rstared::AsRefRTree;
+
+use crate::unionfind::UnionFind;
+
+#[cfg(feature = "std")]
+extern crate std;
+
+// No feature for `alloc` because it would be always enabled anyway.
+extern crate alloc;
+
+mod unionfind;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Point2<K> {
+    x: K,
+    y: K,
+}
+
+impl<K: Copy> From<[K; 2]> for Point2<K> {
+    #[inline]
+    fn from(coords: [K; 2]) -> Self {
+        Self {
+            x: coords[0],
+            y: coords[1],
+        }
+    }
+}
+
+impl<K: FromPrimitive + ToPrimitive> FloatPointCompatible<f64> for Point2<K>
+where
+    Point2<K>: Copy,
+{
+    #[inline]
+    fn from_xy(x: f64, y: f64) -> Self {
+        Self {
+            x: K::from_f64(x).unwrap(),
+            y: K::from_f64(y).unwrap(),
+        }
+    }
+
+    #[inline]
+    fn x(&self) -> f64 {
+        self.x.to_f64().unwrap()
+    }
+
+    #[inline]
+    fn y(&self) -> f64 {
+        self.y.to_f64().unwrap()
+    }
+}
+
+pub struct PolygonUnionFind<
+    K: RTreeNum,
+    PC: KeyedCollection = Vec<Vec<Point2<K>>>,
+    PR: KeyedCollection = AsRefRTree<GeomWithData<Rectangle<[K; 2]>, usize>>,
+    UFPC = Vec<usize>,
+    UFRC = Vec<usize>,
+> {
+    polygons: PC,
+    rtree: PR,
+    unionfind: UnionFind<UFPC, UFRC>,
+    scalar_marker: PhantomData<K>,
+}
+
+impl<
+    K: RTreeNum,
+    PC: Default + KeyedCollection,
+    PR: Default + KeyedCollection,
+    UFPC: Default,
+    UFRC: Default,
+> PolygonUnionFind<K, PC, PR, UFPC, UFRC>
+{
+    #[inline]
+    pub fn new() -> Self {
+        Self {
+            polygons: Default::default(),
+            rtree: Default::default(),
+            unionfind: UnionFind::new(),
+            scalar_marker: PhantomData,
+        }
+    }
+}
+
+impl<K: RTreeNum, PC: KeyedCollection, PR: KeyedCollection, UFPC, UFRC>
+    PolygonUnionFind<K, PC, PR, UFPC, UFRC>
+{
+    #[inline]
+    pub fn polygons(&self) -> &PC {
+        &self.polygons
+    }
+
+    #[inline]
+    pub fn rtree(&self) -> &PR {
+        &self.rtree
+    }
+
+    #[inline]
+    pub fn unionfind(&self) -> &UnionFind<UFPC, UFRC> {
+        &self.unionfind
+    }
+
+    /// Dissolve the polygon-union-find, returning its internal polygons,
+    /// R-tree, and union-find, ceding ownership over them.
+    #[inline]
+    pub fn dissolve(self) -> (PC, PR, UnionFind<UFPC, UFRC>) {
+        (self.polygons, self.rtree, self.unionfind)
+    }
+}
+
+impl<
+    K: FromPrimitive + RTreeNum + ToPrimitive,
+    PC: Get<usize, Value = Vec<Point2<K>>> + Push<usize> + Set<usize>,
+    PR: AsRef<RTree<GeomWithData<Rectangle<[K; 2]>, usize>>>
+        + Insert<GeomWithData<Rectangle<[K; 2]>, usize>, Value = ()>,
+    UFPC: Get<usize, Value = usize> + Push<usize> + Set<usize>,
+    UFRC: Get<usize, Value = usize> + Push<usize> + Set<usize>,
+> PolygonUnionFind<K, PC, PR, UFPC, UFRC>
+where
+    Point2<K>: Copy,
+{
+    pub fn insert(&mut self, polygon: impl IntoIterator<Item = [K; 2]>) {
+        let new_polygon_index = self.unionfind.new_set();
+
+        let polygon: Vec<Point2<K>> = polygon.into_iter().map(Into::into).collect();
+        let rectangle = Self::rectangle_from_polygon(&polygon);
+        self.rtree
+            .insert(GeomWithData::new(rectangle.clone(), new_polygon_index), ());
+
+        self.polygons.push(polygon.clone());
+
+        for neighbor in self
+            .rtree
+            .as_ref()
+            .locate_in_envelope_intersecting(&rectangle.envelope())
+        {
+            let new_polygon_representative = self.unionfind.find(new_polygon_index);
+            let neighbor_representative = self.unionfind.find(neighbor.data);
+            let union = polygon.overlay(
+                self.polygons.get(&neighbor_representative).unwrap(),
+                OverlayRule::Union,
+                FillRule::EvenOdd,
+            );
+
+            if let Some(union) = union.get(&0) {
+                self.polygons.set(neighbor_representative, union[0].clone());
+                self.unionfind
+                    .union(new_polygon_representative, neighbor_representative);
+            }
+        }
+    }
+
+    fn rectangle_from_polygon(vertices: &[Point2<K>]) -> Rectangle<[K; 2]> {
+        Rectangle::from_aabb(
+            vertices
+                .into_iter()
+                .fold(AABB::new_empty(), |aabb, vertex| {
+                    aabb.merged(&AABB::from_point([vertex.x, vertex.y]))
+                }),
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO: tests.
+}
---
+++ src/unionfind.rs
@@ -0,0 +1,187 @@
+// SPDX-FileCopyrightText: 2026 polygon_unionfind contributors
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use maplike::{Get, Push, Set};
+
+/// Disjoint-set union data structure, also known as Union-Find.
+pub struct UnionFind<PC = Vec<usize>, RC = PC> {
+    /// `parents[i]` is the parent of node `i`.
+    parents: PC,
+    /// `ranks[i]` is the upper bound of tree height for node `i`.
+    ranks: RC,
+}
+
+impl<
+    PC: Get<usize, Value = usize> + FromIterator<usize> + Push<usize> + Set<usize>,
+    RC: Get<usize, Value = usize> + FromIterator<usize> + Push<usize> + Set<usize>,
+> UnionFind<PC, RC>
+{
+    /// Create a new `UnionFind` with `size` elements (0..size).
+    #[inline]
+    pub fn with_len(len: usize) -> Self {
+        Self::from_parents_ranks(
+            PC::from_iter(0..len),
+            RC::from_iter(std::iter::repeat(0).take(len)),
+        )
+    }
+}
+
+impl<PC: Default, RC: Default> UnionFind<PC, RC> {
+    #[inline]
+    pub fn new() -> Self {
+        Self::from_parents_ranks(Default::default(), Default::default())
+    }
+}
+
+impl<PC, RC> UnionFind<PC, RC> {
+    /// Create a new `UnionFind` from given parent and rank collections.
+    #[inline]
+    pub fn from_parents_ranks(parents: PC, ranks: RC) -> Self {
+        Self { parents, ranks }
+    }
+
+    /// Dissolve the polygon-union-find, returning its parent and rank
+    /// collections, ceding ownership over them.
+    #[inline]
+    pub fn dissolve(self) -> (PC, RC) {
+        (self.parents, self.ranks)
+    }
+}
+
+impl<
+    PC: Get<usize, Value = usize> + Push<usize> + Set<usize>,
+    RC: Get<usize, Value = usize> + Push<usize> + Set<usize>,
+> UnionFind<PC, RC>
+{
+    pub fn new_set(&mut self) -> usize {
+        let new_set_index = self.ranks.push(0);
+        self.parents.push(new_set_index);
+
+        new_set_index
+    }
+
+    /// Find the representative of `x`, performing path compression along the
+    /// way.
+    pub fn find(&mut self, x: usize) -> usize {
+        if *self.parents.get(&x).unwrap() != x {
+            // Perform the path compression.
+            let parent = self.find(*self.parents.get(&x).unwrap());
+            self.parents.set(x, parent);
+        }
+
+        *self.parents.get(&x).unwrap()
+    }
+
+    /// Unionize the sets containing `x` and `y`, minimizing the rank.
+    ///
+    /// Returns true if merged, false if already in the same set.
+    pub fn union(&mut self, x: usize, y: usize) -> bool {
+        let mut x_representative = self.find(x);
+        let mut y_representative = self.find(y);
+
+        if x_representative == y_representative {
+            return false; // Already connected.
+        }
+
+        // Perform union by rank.
+
+        if self.ranks.get(&x_representative).unwrap() < self.ranks.get(&y_representative).unwrap() {
+            std::mem::swap(&mut x_representative, &mut y_representative);
+        }
+
+        self.parents.set(y_representative, x_representative);
+
+        if self.ranks.get(&x_representative).unwrap() == self.ranks.get(&y_representative).unwrap()
+        {
+            let rank = *self.ranks.get(&x_representative).unwrap();
+            self.ranks.set(x_representative, rank + 1);
+        }
+
+        true
+    }
+
+    /// Check if `x` and `y` are in the same set.
+    pub fn connected(&mut self, x: usize, y: usize) -> bool {
+        self.find(x) == self.find(y)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let unionfind: UnionFind<Vec<usize>> = UnionFind::with_len(5);
+        for i in 0..5 {
+            assert_eq!(unionfind.parents[i], i);
+        }
+    }
+
+    #[test]
+    fn test_new_set() {
+        let mut unionfind: UnionFind<Vec<usize>> = UnionFind::new();
+
+        // First set.
+        let s0 = unionfind.new_set();
+        assert_eq!(s0, 0);
+        assert_eq!(*unionfind.parents.get(&s0).unwrap(), s0);
+        assert_eq!(*unionfind.ranks.get(&s0).unwrap(), 0);
+
+        // Second set.
+        let s1 = unionfind.new_set();
+        assert_eq!(s1, 1);
+        assert_eq!(*unionfind.parents.get(&s1).unwrap(), s1);
+        assert_eq!(*unionfind.ranks.get(&s1).unwrap(), 0);
+
+        // Make sure the two sets are not connected.
+        assert!(!unionfind.connected(s0, s1));
+    }
+
+    #[test]
+    fn test_union_idempotence() {
+        let mut unionfind: UnionFind<Vec<usize>> = UnionFind::with_len(3);
+
+        unionfind.union(0, 1);
+        let representative_before = unionfind.find(0);
+
+        unionfind.union(0, 1); // Perform union on the same pair again.
+        let representative_after = unionfind.find(1);
+
+        assert_eq!(representative_before, representative_after);
+    }
+
+    #[test]
+    fn test_union_and_find() {
+        let mut unionfind: UnionFind<Vec<usize>> = UnionFind::with_len(5);
+        unionfind.union(0, 1);
+        unionfind.union(1, 2);
+
+        let representative0 = unionfind.find(0);
+        let representative1 = unionfind.find(1);
+        let representative2 = unionfind.find(2);
+
+        // The first three elements should have the same representative.
+        assert_eq!(representative0, representative1);
+        assert_eq!(representative1, representative2);
+
+        // 3 and 4 are however still separate.
+        assert_ne!(unionfind.find(3), representative0);
+        assert_ne!(unionfind.find(4), representative0);
+    }
+
+    #[test]
+    fn test_connected() {
+        let mut unionfind: UnionFind<Vec<usize>> = UnionFind::with_len(4);
+        unionfind.union(0, 1);
+        unionfind.union(2, 3);
+
+        assert!(unionfind.connected(0, 1));
+        assert!(unionfind.connected(2, 3));
+        assert!(!unionfind.connected(0, 2));
+
+        unionfind.union(1, 2);
+        assert!(unionfind.connected(0, 3));
+    }
+}
